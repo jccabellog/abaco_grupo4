a3d038fd4fa159d575c03d93226a35e96aba8b99
jccg <=>
2020-11-10 18:41:21 -0300

    se descompone numero ingresado por el usuario

--- abaco.py
+++ abaco.py
@@ -1,1 +1,26 @@
 #Abaco grupo4, Awakelab
+print("########BIENVENIDOS AL ABA-WAKE####### \n")
+numero_separado = []
+ingreso = input("Ingrese algún número : ")
+numero_inicial = int(ingreso)
+
+centena_mil = numero_inicial//100000
+resto = numero_inicial%100000
+decena_mil = resto//10000
+resto_dec = resto%10000
+unidad_mil = resto_dec//1000
+resto_mil = resto_dec%1000
+centena = resto_mil//100
+resto_centena = resto_mil%100
+decena = resto_centena//10
+unidad = resto_centena%10
+
+print ("el numero tiene :")
+print("unidad ",unidad)
+print("decena ",decena)
+print("centena ",centena)
+print("unidad de mil :",unidad_mil)
+print("decena de mil : ",decena_mil)
+print("centena de mil : ",centena_mil)
+
+     
